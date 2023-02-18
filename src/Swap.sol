4527d54106e26758e7af5a120abc1fdeb96a6d8d
SATYAJEET SINDHIYANI <samxan2014@gmail.com>
2023-02-19 01:31:14 +0530

    chore: Removed redundant interfaces and the related functions

--- src/Swap.sol
+++ src/Swap.sol
@@ -35,7 +35,7 @@ contract Swap{
         } 
     }
 
-    // TODO: need to query this with GraphQL
+    // TODO: need to query this with GraphQL/Javascript
     function calculateMinimumFeeForPair(address tokenIn,address tokenOut) view private returns(uint24 minimumFee,address poolAddress){
         for(uint i;i<4;i++){
             poolAddress = factory.getPool(tokenIn, tokenOut,getFeeLevelByValue(FeeLevel(i)));
@@ -99,31 +99,7 @@ interface ISwapRouter {
 }
 
 interface IERC20 {
-    function totalSupply() external view returns (uint);
-
-    function balanceOf(address account) external view returns (uint);
-
-    function transfer(address recipient, uint amount) external returns (bool);
-
-    function allowance(address owner, address spender) external view returns (uint);
-
     function approve(address spender, uint amount) external returns (bool);
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint amount
-    ) external returns (bool);
-
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
-    function decimals() external view returns (uint8);
-}
-
-interface IWETH is IERC20 {
-    function deposit() external payable;
-
-    function withdraw(uint amount) external;
 }
 
 
@@ -132,40 +108,7 @@ pragma solidity >=0.5.0;
 /// @title The interface for the Uniswap V3 Factory
 /// @notice The Uniswap V3 Factory facilitates creation of Uniswap V3 pools and control over the protocol fees
 interface IUniswapV3Factory {
-    /// @notice Emitted when the owner of the factory is changed
-    /// @param oldOwner The owner before the owner was changed
-    /// @param newOwner The owner after the owner was changed
-    event OwnerChanged(address indexed oldOwner, address indexed newOwner);
-
-    /// @notice Emitted when a pool is created
-    /// @param token0 The first token of the pool by address sort order
-    /// @param token1 The second token of the pool by address sort order
-    /// @param fee The fee collected upon every swap in the pool, denominated in hundredths of a bip
-    /// @param tickSpacing The minimum number of ticks between initialized ticks
-    /// @param pool The address of the created pool
-    event PoolCreated(
-        address indexed token0,
-        address indexed token1,
-        uint24 indexed fee,
-        int24 tickSpacing,
-        address pool
-    );
-
-    /// @notice Emitted when a new fee amount is enabled for pool creation via the factory
-    /// @param fee The enabled fee, denominated in hundredths of a bip
-    /// @param tickSpacing The minimum number of ticks between initialized ticks for pools created with the given fee
-    event FeeAmountEnabled(uint24 indexed fee, int24 indexed tickSpacing);
-
-    /// @notice Returns the current owner of the factory
-    /// @dev Can be changed by the current owner via setOwner
-    /// @return The address of the factory owner
-    function owner() external view returns (address);
-
-    /// @notice Returns the tick spacing for a given fee amount, if enabled, or 0 if not enabled
-    /// @dev A fee amount can never be removed, so this value should be hard coded or cached in the calling context
-    /// @param fee The enabled fee, denominated in hundredths of a bip. Returns 0 in case of unenabled fee
-    /// @return The tick spacing
-    function feeAmountTickSpacing(uint24 fee) external view returns (int24);
+    
 
     /// @notice Returns the pool address for a given pair of tokens and a fee, or address 0 if it does not exist
     /// @dev tokenA and tokenB may be passed in either token0/token1 or token1/token0 order
@@ -178,80 +121,6 @@ interface IUniswapV3Factory {
         address tokenB,
         uint24 fee
     ) external view returns (address pool);
-
-    /// @notice Creates a pool for the given two tokens and fee
-    /// @param tokenA One of the two tokens in the desired pool
-    /// @param tokenB The other of the two tokens in the desired pool
-    /// @param fee The desired fee for the pool
-    /// @dev tokenA and tokenB may be passed in either order: token0/token1 or token1/token0. tickSpacing is retrieved
-    /// from the fee. The call will revert if the pool already exists, the fee is invalid, or the token arguments
-    /// are invalid.
-    /// @return pool The address of the newly created pool
-    function createPool(
-        address tokenA,
-        address tokenB,
-        uint24 fee
-    ) external returns (address pool);
-
-    /// @notice Updates the owner of the factory
-    /// @dev Must be called by the current owner
-    /// @param _owner The new owner of the factory
-    function setOwner(address _owner) external;
-
-    /// @notice Enables a fee amount with the given tickSpacing
-    /// @dev Fee amounts may never be removed once enabled
-    /// @param fee The fee amount to enable, denominated in hundredths of a bip (i.e. 1e-6)
-    /// @param tickSpacing The spacing between ticks to be enforced for all pools created with the given fee amount
-    function enableFeeAmount(uint24 fee, int24 tickSpacing) external;
+    
 }
 
-pragma solidity >=0.7.5;
-pragma abicoder v2;
-
-/// @title Quoter Interface
-/// @notice Supports quoting the calculated amounts from exact input or exact output swaps
-/// @dev These functions are not marked view because they rely on calling non-view functions and reverting
-/// to compute the result. They are also not gas efficient and should not be called on-chain.
-interface IQuoter {
-    /// @notice Returns the amount out received for a given exact input swap without executing the swap
-    /// @param path The path of the swap, i.e. each token pair and the pool fee
-    /// @param amountIn The amount of the first token to swap
-    /// @return amountOut The amount of the last token that would be received
-    function quoteExactInput(bytes memory path, uint256 amountIn) external returns (uint256 amountOut);
-
-    /// @notice Returns the amount out received for a given exact input but for a swap of a single pool
-    /// @param tokenIn The token being swapped in
-    /// @param tokenOut The token being swapped out
-    /// @param fee The fee of the token pool to consider for the pair
-    /// @param amountIn The desired input amount
-    /// @param sqrtPriceLimitX96 The price limit of the pool that cannot be exceeded by the swap
-    /// @return amountOut The amount of `tokenOut` that would be received
-    function quoteExactInputSingle(
-        address tokenIn,
-        address tokenOut,
-        uint24 fee,
-        uint256 amountIn,
-        uint160 sqrtPriceLimitX96
-    ) external returns (uint256 amountOut);
-
-    /// @notice Returns the amount in required for a given exact output swap without executing the swap
-    /// @param path The path of the swap, i.e. each token pair and the pool fee
-    /// @param amountOut The amount of the last token to receive
-    /// @return amountIn The amount of first token required to be paid
-    function quoteExactOutput(bytes memory path, uint256 amountOut) external returns (uint256 amountIn);
-
-    /// @notice Returns the amount in required to receive the given exact output amount but for a swap of a single pool
-    /// @param tokenIn The token being swapped in
-    /// @param tokenOut The token being swapped out
-    /// @param fee The fee of the token pool to consider for the pair
-    /// @param amountOut The desired output amount
-    /// @param sqrtPriceLimitX96 The price limit of the pool that cannot be exceeded by the swap
-    /// @return amountIn The amount required as the input for the swap in order to receive `amountOut`
-    function quoteExactOutputSingle(
-        address tokenIn,
-        address tokenOut,
-        uint24 fee,
-        uint256 amountOut,
-        uint160 sqrtPriceLimitX96
-    ) external returns (uint256 amountIn);
-}
